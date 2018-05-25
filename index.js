64cccf772e81a5419ec0fee4d80c284d3e0858be
auraarias <you@example.comauraarias1996@gmail.comgit config --global user.name auraariasgit config --global user.email you@example.comauraarias1996@gmail.com>
2018-05-25 11:37:40 -0500

    boton

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require ("express"),
+        consolidate = require ("consolidate"),
+        handlebars = require ("handlebars"),
+        
+
+    var app = express(),
+    db;
+
+    app.engine("hbs", consolidate.handlebars);
+
+    app.set('views', './views');
+    app.set('view engine', 'hbs');
+
+    app.use(express.static('public'));
+
+    document.querySelectorAll('.agregar').forEach(function(button) {
+        button.addEventListener('click', function(){
+            var id = button.parentElement.getAttribute('data-id');
+    
+            if(arreglo.indexOf(id) > 0){
+                return;
+            }
+    
+            arreglo.push(id);
+            actualizarVisitas();
+    
+            localStorage.setItem('arreglo', JSON.stringify(arreglo));
+        });
+    });
+
+    function actualizarVisitas (){
+        document.querySelector('.visita').innerHTML = arreglo.length;
+    }
+    
+    var arreglo = JSON.parse(localStorage.getItem('arreglo'));
+    if(arreglo == null) arreglo = [];
+    localStorage.clear('arreglo');
+    
+    actualizarVisitas();
